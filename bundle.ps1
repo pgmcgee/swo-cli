6c652cc9a2ed0ebe3c2773b6bbc10223b2eb2a36
Parker McGee <parker@thosemcgees.com>
2021-01-30 08:40:14 -0500

    Add bundle.ps1

---
+++ bundle.ps1
@@ -0,0 +1,12 @@
+# Create build directory if it doesn't exist
+New-Item -ItemType Directory -Path .\build -Force
+
+# Copy all plugins into build
+Copy-Item -Path .\plugins -Destination .\build -Recurse -Force
+
+# Copy in the built exe
+Copy-Item -Path .\bin\Release\net5.0\win10-x64\swo-cli.exe -Destination .\build -Force
+
+# Create zip archive
+Remove-Item -Path .\build.zip -Force -ErrorAction Ignore
+Compress-Archive -Path .\build -DestinationPath .\build.zip 
