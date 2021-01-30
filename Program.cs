59e5080be06752aadac68d38394c17e504c1cff2
Parker McGee <parker@thosemcgees.com>
2021-01-30 07:38:54 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using System.IO;
+
+namespace swo_cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+
+            var pluginFilenames = Directory.GetFiles(@"./plugins", "*.dll");
+
+            foreach(string pluginFilename in pluginFilenames) {
+                var pluginPath = Path.GetFullPath(pluginFilename, Directory.GetCurrentDirectory());
+                var plugin = Assembly.LoadFile(pluginPath);
+
+                foreach(Type type in plugin.GetExportedTypes()) {
+                    dynamic c = Activator.CreateInstance(type);
+                    c.Run();
+                }
+            }
+
+            Console.WriteLine("Done");
+        }
+    }
+}
